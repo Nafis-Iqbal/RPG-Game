200a506595e8ba3051c4707a7910aaa0aa46be7b
Nafis-Iqbal <nafisiqbal10053@gmail.com>
2021-02-04 23:48:09 +0600

    redone

--- 2D-RPG new/Assets/Scripts/MyScripts/GameSystems/InventorySystem/pickUpInfoScript.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class pickUpInfoScript : MonoBehaviour
-{
-    public static playerPickUpClass[] pickUpInfoTable = new playerPickUpClass[20];
-}
